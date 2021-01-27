c279325df0986df57f180951fa91ece431b6936d
Thiago Borges Vieira <tborgesvieira@gmail.com>
2021-01-27 15:04:00 -0300

    Arquivos criados na aula 1

--- aula1/pages/room/src/business.js
+++ aula1/pages/room/src/business.js
@@ -1,8 +1,15 @@
 class Business {
-    constructor({room, media, view}){
+    constructor({room, media, view, socketBuilder}){
         this.room = room;
         this.media = media;
         this.view = view;
+        
+        this.socketBuilder = socketBuilder
+                                .setOnUserConnected(this.onUserConnected())
+                                .setOnUserDisconnected(this.onUserDisconnected())
+                                .build();
+
+        this.socketBuilder.emit('join-room', this.room, 'teste01');
 
         this.currentStream = {};
     }
@@ -25,5 +32,17 @@ class Business {
             stream,
             isCurrentId
         });
+    }
+
+    onUserConnected = function(){
+        return userId =>{
+            console.log('user connected!', userId);
+        }
+    }
+
+    onUserDisconnected = function(){
+        return userId =>{
+            console.log('user disconnected!', userId);
+        }
     }
 }
